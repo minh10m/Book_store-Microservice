e65f600f612636ea41215d1940508fab59480677
minh10m <minhhh1010@gmail.com>
2025-12-06 21:40:47 +0700

    feat: add integration test controller and repository of product

--- src/test/java/microservice/catalog/CatalogApplicationTests.java
+++ src/test/java/microservice/catalog/CatalogApplicationTests.java
@@ -1,12 +1,8 @@
 package microservice.catalog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class CatalogApplicationTests {
+class CatalogApplicationTests extends AbstractIT {
 
     @Test
     void contextLoads() {}
