47f4704a3f5de3478564043a94cf55e1c3766ee2
minh10m <minhhh1010@gmail.com>
2025-12-08 20:25:46 +0700

    feat(order service): add controller, service, repo

---
+++ order-service/src/main/java/microservice/order_service/application/SecurityService.java
@@ -0,0 +1,10 @@
+package microservice.order_service.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+    public String getLoginUserName(){
+        return "user";
+    }
+}
