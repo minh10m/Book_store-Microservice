7511283f19de6defd40140ab36d0d5641183fd53
minh10m <minhhh1010@gmail.com>
2025-12-08 23:01:33 +0700

    feat: add wiremock test container

--- order-service/src/test/java/microservice/order_service/AbstractIT.java
+++ order-service/src/test/java/microservice/order_service/AbstractIT.java
@@ -1,19 +1,21 @@
 package microservice.order_service;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import io.restassured.RestAssured;
 import java.math.BigDecimal;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
@@ -22,24 +24,37 @@ public abstract class AbstractIT {
     @LocalServerPort
     int port;
 
+    public static WireMockContainer wiremockServer = new WireMockContainer("wiremock/wiremock:3.5.2-alpine");
+
+    @BeforeAll
+    static void beforeAll(){
+        wiremockServer.start();
+        configureFor(wiremockServer.getHost(), wiremockServer.getPort());
+    }
+
+
+    static void configureProperties(DynamicPropertyRegistry registry){
+        registry.add("orders.catalog-service.url", wiremockServer::getBaseUrl);
+    }
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
     }
 
     protected void mockGetProductByCode(String code, String name, BigDecimal price) {
-        stubFor(get(urlMatching("/api/products/" + code))
+        stubFor(WireMock.get(urlMatching("/api/products/" + code))
                 .willReturn(aResponse()
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                        .withStatus(200)
-                        .withBody(
-                                """
-                                {
-                                    "code": "%s",
-                                    "name": "%s",
-                                    "price": %f
-                                }
-                                """
-                                        .formatted(code, name, price.doubleValue()))));
+                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .withStatus(200)
+                .withBody(
+                        """
+                        {
+                            "code": "%s",
+                            "name": "%s",
+                            "price": %f
+                        }
+                        """
+                .formatted(code, name, price.doubleValue()))));
     }
 }
