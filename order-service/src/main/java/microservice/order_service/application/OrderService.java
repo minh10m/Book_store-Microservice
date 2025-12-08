47d8389eb70384f20010835a634f76c9eb44536d
minh10m <minhhh1010@gmail.com>
2025-12-08 23:03:28 +0700

    feat: add scheduled job and scheduler lock

--- order-service/src/main/java/microservice/order_service/application/OrderService.java
+++ order-service/src/main/java/microservice/order_service/application/OrderService.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 import microservice.order_service.adapters.persistent.OrderRepository;
 import microservice.order_service.adapters.web.dto.CreateOrderRequest;
 import microservice.order_service.adapters.web.dto.CreateOrderResponse;
+import microservice.order_service.adapters.web.dto.OrderCreatedEvent;
 import microservice.order_service.adapters.web.dto.OrderDTO;
 import microservice.order_service.adapters.web.mapper.OrderMapper;
 import microservice.order_service.domain.OrderEntity;
 import microservice.order_service.domain.OrderValidator;
+import microservice.order_service.domain.model.OrderStatus;
 import microservice.order_service.domain.model.OrderSummary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,18 +26,13 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository, OrderValidator orderValidator) {
+    public OrderService(OrderRepository orderRepository, OrderValidator orderValidator, OrderEventService orderEventService) {
         this.orderRepository = orderRepository;
         this.orderValidator = orderValidator;
+        this.orderEventService = orderEventService;
     }
     private final OrderValidator orderValidator;
-//    private final OrderEventService orderEventService;
-
-//    OrderService(OrderRepository orderRepository, OrderValidator orderValidator, OrderEventService orderEventService) {
-//        this.orderRepository = orderRepository;
-//        this.orderValidator = orderValidator;
-//        this.orderEventService = orderEventService;
-//    }
+    private final OrderEventService orderEventService;
 
     public CreateOrderResponse createOrder(String userName, CreateOrderRequest request) {
         orderValidator.validate(request);
@@ -43,8 +40,8 @@ public class OrderService {
         newOrder.setUserName(userName);
         OrderEntity savedOrder = this.orderRepository.save(newOrder);
         log.info("Created Order with orderNumber={}", savedOrder.getOrderNumber());
-//        OrderCreatedEvent orderCreatedEvent = OrderEventMapper.buildOrderCreatedEvent(savedOrder);
-//        orderEventService.save(orderCreatedEvent);
+        OrderCreatedEvent orderCreatedEvent = OrderEventMapper.buildOrderCreatedEvent(savedOrder);
+        orderEventService.save(orderCreatedEvent);
         return new CreateOrderResponse(savedOrder.getOrderNumber());
     }
 
@@ -58,33 +55,33 @@ public class OrderService {
                 .map(OrderMapper::convertToDTO);
     }
 
-//    public void processNewOrders() {
-//        List<OrderEntity> orders = orderRepository.findByStatus(OrderStatus.NEW);
-//        log.info("Found {} new orders to process", orders.size());
-//        for (OrderEntity order : orders) {
-//            this.process(order);
-//        }
-//    }
+    public void processNewOrders() {
+        List<OrderEntity> orders = orderRepository.findByStatus(OrderStatus.NEW);
+        log.info("Found {} new orders to process", orders.size());
+        for (OrderEntity order : orders) {
+            this.process(order);
+        }
+    }
 
-//    private void process(OrderEntity order) {
-//        try {
-//            if (canBeDelivered(order)) {
-//                log.info("OrderNumber: {} can be delivered", order.getOrderNumber());
-//                orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.DELIVERED);
-//                orderEventService.save(OrderEventMapper.buildOrderDeliveredEvent(order));
-//
-//            } else {
-//                log.info("OrderNumber: {} can not be delivered", order.getOrderNumber());
-//                orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.CANCELLED);
-//                orderEventService.save(
-//                        OrderEventMapper.buildOrderCancelledEvent(order, "Can't deliver to the location"));
-//            }
-//        } catch (RuntimeException e) {
-//            log.error("Failed to process Order with orderNumber: {}", order.getOrderNumber(), e);
-//            orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.ERROR);
-//            orderEventService.save(OrderEventMapper.buildOrderErrorEvent(order, e.getMessage()));
-//        }
-//    }
+    private void process(OrderEntity order) {
+        try {
+            if (canBeDelivered(order)) {
+                log.info("OrderNumber: {} can be delivered", order.getOrderNumber());
+                orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.DELIVERED);
+                orderEventService.save(OrderEventMapper.buildOrderDeliveredEvent(order));
+
+            } else {
+                log.info("OrderNumber: {} can not be delivered", order.getOrderNumber());
+                orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.CANCELLED);
+                orderEventService.save(
+                        OrderEventMapper.buildOrderCancelledEvent(order, "Can't deliver to the location"));
+            }
+        } catch (RuntimeException e) {
+            log.error("Failed to process Order with orderNumber: {}", order.getOrderNumber(), e);
+            orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.ERROR);
+            orderEventService.save(OrderEventMapper.buildOrderErrorEvent(order, e.getMessage()));
+        }
+    }
 
     private boolean canBeDelivered(OrderEntity order) {
         return DELIVERY_ALLOWED_COUNTRIES.contains(
