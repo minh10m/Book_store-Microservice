82a530d9804619ecd7e73daac20806ebfe9e7e58
minh10m <minhhh1010@gmail.com>
2025-12-09 19:31:34 +0700

    feat: add event model

---
+++ notification-service/src/main/java/microservice/notification_service/domain/model/OrderCancelledEvent.java
@@ -0,0 +1,13 @@
+package microservice.notification_service.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record OrderCancelledEvent(
+        String eventId,
+        String orderNumber,
+        Set<OrderItem> items,
+        Customer customer,
+        Address deliveryAddress,
+        String reason,
+        LocalDateTime createdAt) {}
