72d248abc168705c335d30952c46693782a801a9
minh10m <minhhh1010@gmail.com>
2025-12-15 17:00:50 +0700

    refactor: fix

--- code/web-app/src/test/java/com/example/web_app/WebAppApplicationTests.java
+++ code/web-app/src/test/java/com/example/web_app/WebAppApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@org.springframework.context.annotation.Import(TestcontainersConfiguration.class)
 class WebAppApplicationTests {
 
     @Test
