56d443c942d3748189fdd297de34087669ec3bce
minh10m <minhhh1010@gmail.com>
2026-04-24 20:56:30 +0700

    test: remove redundant TestcontainersConfiguration import from application tests

--- code/web-app/src/test/java/com/example/web_app/WebAppApplicationTests.java
+++ code/web-app/src/test/java/com/example/web_app/WebAppApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@org.springframework.context.annotation.Import(TestcontainersConfiguration.class)
 class WebAppApplicationTests {
 
     @Test
