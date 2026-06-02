bb352d6a7fc0268ac4727d3d47a8c379e43e16ab
minh10m <minhhh1010@gmail.com>
2026-06-02 23:19:14 +0700

    chore: fix Docker image tagging in CI workflows and mock ClientRegistrationRepository in test suite

--- code/web-app/src/test/java/com/example/web_app/WebAppApplicationTests.java
+++ code/web-app/src/test/java/com/example/web_app/WebAppApplicationTests.java
@@ -2,9 +2,14 @@ package com.example.web_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @SpringBootTest
 class WebAppApplicationTests {
+
+    @MockBean
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Test
     void contextLoads() {}
